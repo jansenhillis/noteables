4243381666e44311d6b05809542765bc8d35858b
Saif Ahmed <saifkhajaahmed@gmail.com>
2021-03-31 21:51:27 -0700

    Created User model, urls, and views. Populated all 3 files with working back end code base.

--- django_files/noteables/models.py
+++ django_files/noteables/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from datetime import datetime
+from login.models import User
+import re
 
 # Create your models here.
-# Hello World
+
+class NoteManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+        if len(postData['note_content']) < 1:
+            errors['note_content'] = 'Something needs to be written in the notes content.'
+        if len(postData['note_title']) < 1:
+            errors['note_title'] = 'Note needs a title.'
+        return errors
+
+
+class Note(models.Model):
+    created_by = models.ForeignKey(User, related_name="notes_created", on_delete=models.CASCADE)
+    note_content = models.CharField(max_length=1000)
+    note_title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = NoteManager()
+
+    def __repr__(self):
+        return f"<Note object: ({self.id}) {self.note_title}"
--- django_files/noteables/views.py
+++ django_files/noteables/views.py
@@ -1,6 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from login.models import *
+from .models import *
 
-# Create your views here.
+
 # @authenticate_user
 def index(request):
     return render(request, 'index.html')
+
+
+#opens a specific note to view or edit
+def open_note(request, id):
+    context = {
+        'note': Note.objects.get(id=id),
+        'user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, "open_note.html", context)    
+
+
+#updates note if any edits were made - to be added when we add in TinyMCE implementation
+# def update_note(request):
+#     return redirect('/')
+
+
+#deletes a note from the database on dashboard
+def delete_note(request, id):
+    Note.objects.get(id=id).delete()
+    return redirect('/')
+
+
+#opens a new note
+def new_note(request):
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+    }
+    return render(request, "new_note.html", context)
+
+
+#saves newly opened note 
+def save_note(request):
+    errors = Note.objects.validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/new_note')
+
+    user = User.objects.get(id=request.session['user_id'])
+    note = Note.objects.create(note_title=request.POST['note_title'], note_content=request.POST['note_content'], created_by= user)
+    return redirect('/')
+
+
+#goes to homepage 
+def dashboard(request):
+    return redirect('/')
+
+
+#logs out user and returns to login screen - WIP
+def logout(request):
+    request.session.clear()
+    return redirect('/') #TODO: Determine which url path to put here
